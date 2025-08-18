974519e10261ee2da787f560c96fc508453067aa
Shiva Billana <billanashivakumar@gmail.com>
2025-08-18 22:46:58 +0530

    Updated the main.py and Main.ipynb

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # %%
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error,root_mean_squared_error
+from sklearn.metrics import mean_squared_error
 
 # %%
 dataset = pd.read_csv('housing_price_dataset.csv')
@@ -22,20 +23,20 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
 
 # %%
 sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
+x_train_sc = sc.fit_transform(x_train)
+x_test_sc = sc.transform(x_test)
 
 # %%
 regressor = LinearRegression()
-regressor.fit(x_train,y_train)
+regressor.fit(x_train_sc,y_train)
 
 # %%
-y_pred = regressor.predict(x_test)
-print(y_pred)
+y_pred_sc = regressor.predict(x_test_sc)
+print(y_pred_sc)
 
 # %%
-mse = mean_squared_error(y_test,y_pred)
-rmse = root_mean_squared_error(y_test,y_pred)
+mse = mean_squared_error(y_test,y_pred_sc)
+rmse = np.sqrt(mse)
 
 # %%
 print("Mean Squared Error :",mse)
@@ -43,7 +44,7 @@ print("Root Mean Squared Error :",rmse)
 
 # %%
 plt.figure(figsize=(10, 6))
-plt.scatter(y_test, y_pred, color='blue', alpha=0.6)
+plt.scatter(y_test, y_pred_sc, color='blue', alpha=0.6)
 plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
 plt.title('Actual vs Predicted Prices')
 plt.xlabel('Actual Prices')
@@ -51,5 +52,17 @@ plt.ylabel('Predicted Prices')
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+# %%
+print(regressor.coef_)
+print(regressor.intercept_)
+
+# %%
+# %% Print regression equation
+print("Regression Equation:")
+equation = "y = {:.2f}".format(regressor.intercept_)
+for i, coef in enumerate(regressor.coef_):
+    equation += " + ({:.2f} * x{})".format(coef, i+1)
+print(equation)
 
 
